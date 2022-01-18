31ba26fc44eaca726c21c91caeed7650d1bf5261
Wallace Leonardo <wallacelw22@gmail.com>
2022-01-18 17:46:03 -0300

    Correção
    correção de linha

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toggle = document.getElementById('toggle');
 const sidebar = document.getElementById('sidebar');
 
 document.onclick = function(e){
-    if(e.target.id !== 'sidebar' && e.target.id !== 'toggle'){
+    if(e.target.id !== 'sidebar' && e.target.id !== 'toggle'){ 
         toggle.classList.remove('active');
         sidebar.classList.remove('active');
     }
